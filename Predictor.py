6990a8898a44efc7774007ede0f20c7e8f572a1e
Anastasiya Fedotova <105239022+fadingreflection@users.noreply.github.com>
2024-05-12 13:46:52 +0300

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -2,11 +2,13 @@ import torch
 import pandas as pd
 
 class Predictor():
-    def __init__(self, df_source, target=str):  #binary file  #target to func itself #init ONLY ONCE
+    def __init__(self, df_source):  #binary file  #target to func itself #init ONLY ONCE
         self.df_source=df_source
-        self.target=target
+        self.target=None
         self.model=None
         self.tokenizer=None    
+        self.load_model()
+        self.df_source['embeddings']=self.df_source.vector.apply(lambda x: self.embed_bert_cls(text=x))
         
     def load_model(self):
         import torch
@@ -25,13 +27,11 @@ class Predictor():
         target_embeds = embeddings[0].cpu().numpy()    
         return target_embeds    
           
-    def find_matches(self, n_neighbours=1):
+    def find_matches(self, target, n_neighbours=1):
         import numpy as np
-        self.load_model()
-        target=self.embed_bert_cls(text=self.target)
-        self.df_source['embeddings']=self.df_source.vector.apply(lambda x: self.embed_bert_cls(text=x))
+        target=self.embed_bert_cls(text=target)
+        # self.df_source['embeddings']=self.df_source.vector.apply(lambda x: self.embed_bert_cls(text=x))
         samples=self.df_source['embeddings'].tolist()
-        # print(samples)
         from sklearn.neighbors import NearestNeighbors
         neigh = NearestNeighbors(n_neighbors=n_neighbours)
         neigh.fit(samples)
@@ -40,9 +40,3 @@ class Predictor():
         distance=neigh.kneighbors([target])[0][0]
         res=self.df_source.iloc[indexes]['vector']
         return indexes, res, distance
-    
-
-# df_mts=pd.read_csv('venues_mts.csv', index_col='Unnamed: 0').dropna()
-# df_approved=pd.read_csv('df_orgs.csv')    
-# predictor_instance=Predictor(df_mts, 'Театр «Практика» г. Москва, переулок Большой Козихинский, дом 30')
-# print(predictor_instance.find_matches())
